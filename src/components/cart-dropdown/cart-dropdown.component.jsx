759fc73acf5be8204dc6fe28dc5d420ba8a0982e
radmehrazari <radmehrazari@gmail.com>
2023-03-16 14:46:22 +0330

    checkout tebgh udemy pish raft

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,6 +1,6 @@
 import { useContext } from "react";
 import { CartContext } from "../../contexts/cart.context";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 import Button from "../button/button.component";
 import CartItem from "../cart-item/cart-item.component";
@@ -8,6 +8,11 @@ import "./cart-dropdown.styles.scss";
 
 const CartDropdown = () => {
   const { cartItems } = useContext(CartContext);
+  const navigate = useNavigate();
+
+  const goTOCheckOutHandler = () =>{
+    navigate('/checkout');
+  }
 
   return (
     <div className="cart-dropdown-container">
@@ -16,9 +21,9 @@ const CartDropdown = () => {
           <CartItem key={item.id} CartItem={item} />
         ))}
       </div>
-      <Link to="./checkout">
-        <Button Children="Go To Checkout"></Button>
-      </Link>
+
+        <Button Children="Go To Checkout" onClick={goTOCheckOutHandler}></Button>
+
     </div>
   );
 };
--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,13 +1,5 @@
 import { createContext, useState, useEffect } from "react";
 
-export const CartContext = createContext({
-  isCartOpen: false,
-  addItemToCart: () => {},
-  setIsCartOpen: () => {},
-  cartItems: [],
-  cartCount: 0,
-});
-
 const addCartItem = (cartItems, productToAdd) => {
   const existingCartItem = cartItems.find(
     (cartItem) => cartItem.id === productToAdd.id
@@ -24,10 +16,42 @@ const addCartItem = (cartItems, productToAdd) => {
   return [...cartItems, { ...productToAdd, quantity: 1 }];
 };
 
+const removeCartItem = (cartItems, cartItemToRemove) => {
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === cartItemToRemove.id
+  );
+
+  if (existingCartItem.quantity === 1) {
+    return cartItems.filter((cartItem) => cartItem.id !== cartItemToRemove.id);
+  }
+  // ما همیشه یه آبجکت بر میگردانیم چون میخواهیم مجدد رندر کنیم و پراپس رو تغییر بدیم
+  return cartItems.map((cartItem) =>
+    cartItem.id === cartItemToRemove.id
+      ? { ...cartItem, quantity: cartItem.quantity - 1 }
+      : cartItem
+  );
+};
+
+const clearCartItem = (cartItems, cartItemToClear) => {
+  return cartItems.filter((cartItem) => cartItem.id !== cartItemToClear.id);
+};
+
+export const CartContext = createContext({
+  isCartOpen: false,
+  setIsCartOpen: () => {},
+  cartItems: [],
+  addItemToCart: () => {},
+  removeItemFromCart: () => {},
+  clearItemFromCart: () => {},
+  total: 0,
+  cartCount: 0,
+});
+
 export const CartProvider = ({ children }) => {
   const [isCartOpen, setIsCartOpen] = useState(false);
   const [cartItems, setCartItem] = useState([]);
   const [cartCount, setCartCount] = useState(0);
+  const [total, setTotal] = useState(0);
 
   useEffect(() => {
     setCartCount(
@@ -35,17 +59,34 @@ export const CartProvider = ({ children }) => {
     );
   }, [cartItems]);
 
+  useEffect(() => {
+    setTotal(
+      cartItems.reduce((total, currentElement) => total + currentElement.quantity * currentElement.price, 0)
+    );
+  }, [cartItems]);
+
   const addItemToCart = (productToAdd) => {
     setCartItem(addCartItem(cartItems, productToAdd));
+  };
+
+  const removeItemToCart = (cartItemToRemove) => {
+    setCartItem(removeCartItem(cartItems, cartItemToRemove));
+  };
+
+  const clearItemToCart = (cartItemToClear) => {
+    setCartItem(clearCartItem(cartItems, cartItemToClear));
   };
 
   const value = {
     isCartOpen,
     setIsCartOpen,
     addItemToCart,
+    removeItemToCart,
+    clearItemToCart,
     cartItems,
     cartCount,
-    setCartItem
+    total,
+    setCartItem,
   };
 
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
